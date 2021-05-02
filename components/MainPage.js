2a43df1aa7a0e34369b42c2401ea7c5a3db8a4a4
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2021-05-02 14:05:58 -0400

    I hate this

--- components/MainPage.js
+++ components/MainPage.js
@@ -5,15 +5,33 @@ import { StyleSheet, Text, View, Image, ImageBackground, TouchableHighlight } fr
 export default function MainPage({navigation}) {
   return (
     <View style={styles.container}>
+ 
       <ImageBackground style={{ alignItems: 'center', paddingBottom: 25, justifyContent: "center", width: 414, height: 896 }} 
       source={require('./../images/dashboardPage.png')}>
 
-        <TouchableHighlight onPress={ () => navigation.navigate("signupPage")}> 
+      <View style={{ alignItems: 'center'}}>
+
+      </View>
+
+      <TouchableHighlight onPress= { () => navigation.navigate("signupPage")}>
+          <Image style={{ left: 160, bottom: 375, width: 28.83, height: 27.22 }} 
+              source={require('./../images/logout.png')}>
+          </Image> 
+      </TouchableHighlight>
+
+
+        <TouchableHighlight onPress= { () => navigation.navigate("signupPage")}>
+          <Image style={{width: 394, height: 100 }} 
+              source={require('./../images/linkup.svg')}>
+          </Image> 
+        </TouchableHighlight>
+
+
+        {/* <TouchableHighlight onPress={ () => navigation.navigate("signupPage")}> 
           <Image style={{  width: 190, height: 50 }} 
           source={require('./../images/signin.png')}>
           </Image>
-        </TouchableHighlight>
-
+        </TouchableHighlight> */}
       </ImageBackground>
 
     </View>
