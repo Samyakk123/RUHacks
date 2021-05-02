dfb5a6bf4a46218f0fc228b661f0a8da3d8b5d5b
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2021-05-02 10:00:46 -0400

    Added the back img

--- App.js
+++ App.js
@@ -4,20 +4,22 @@ import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
 
 import Navigator from './routes/homeStack';
 
+import Login from './components/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground style={{ alignItems: 'center', paddingBottom: 25, justifyContent: "center", width: 414, height: 896 }} 
-      source={require('./images/LoginPage.svg')}>
+    <Login></Login>
+    // <View style={styles.container}>
+    //   <ImageBackground style={{ alignItems: 'center', paddingBottom: 25, justifyContent: "center", width: 414, height: 896 }} 
+    //   source={require('./images/LoginPage.svg')}>
 
-        <Image style={{  width: 190, height: 50 }} 
-        source={require('./images/signin.png')}>
-        </Image>
+    //     <Image style={{  width: 190, height: 50 }} 
+    //     source={require('./images/signin.png')}>
+    //     </Image>
 
-      </ImageBackground>
+    //   </ImageBackground>
 
-    </View>
+    // </View>
   );
 }
 
---
+++ components/Login.js
@@ -0,0 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import Navigator from './routes/homeStack';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <ImageBackground style={{ alignItems: 'center', paddingBottom: 25, justifyContent: "center", width: 414, height: 896 }} 
+      source={require('./images/LoginPage.svg')}>
+
+        <Image style={{  width: 190, height: 50 }} 
+        source={require('./images/signin.png')}>
+        </Image>
+
+      </ImageBackground>
+
+    </View>
+  );
+
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+    
+  },
+});
