f29bb58a13de0958e11381349429f556eeaf61f0
Yajurva Trivedi <yajurva.trivedi@ryerson.ca>
2021-05-02 13:08:21 -0400

    sammy

--- components/Login.js
+++ components/Login.js
@@ -1,18 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, ImageBackground, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, TextInput, TouchableHighlight} from 'react-native';
+import { color } from 'react-native-reanimated';
 
 export default function Login({navigation}) {
+ 
+  const [text, onChangeText] = React.useState(null);
+  const [texttwo, onChangeTexttwo] = React.useState(null);
+
   return (
+
+    
+
     <View style={styles.container}>
+      
       <ImageBackground style={{ alignItems: 'center', paddingBottom: 25, justifyContent: "center", width: 414, height: 896 }} 
       source={require('./../images/LoginPage.svg')}>
+        <TextInput style={styles.inputone}  onChangeText={onChangeText} value={text} placeholder="email" placeholderTextColor="grey"/>
+        <TextInput style={styles.input}  onChangeText={onChangeTexttwo} value={texttwo} placeholder="password"/>
 
         <TouchableHighlight onPress={ () => navigation.navigate("mainPage")}> 
           <Image style={{  width: 190, height: 50 }} 
           source={require('./../images/signin.png')}>
           </Image>
         </TouchableHighlight>
+
 
       </ImageBackground>
 
@@ -31,5 +42,44 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     
     
+  },
+  inputone: {
+    paddingLeft:10,
+    display:'flex',
+    marginTop:-130,
+    width: 268,
+    height: 50,
+    borderWidth: 1,
+    backgroundColor: '#F8F8F8',
+    borderRadius: 50,
+    borderColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+	    width: 0,
+	    height: 2,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 2.65,
+    elevation: 5, 
+  },
+  input: {
+    paddingLeft:10,
+    display:'flex',
+    marginTop:15,
+    marginBottom:10,
+    width: 268,
+    height: 50,
+    borderWidth: 1,
+    backgroundColor: '#F8F8F8',
+    borderRadius: 50,
+    borderColor: "white",
+    shadowColor: "#000",
+    shadowOffset: {
+	    width: 0,
+	    height: 2,
+    },
+    shadowOpacity: 0.30,
+    shadowRadius: 2.65,
+    elevation: 5,
   },
 });
